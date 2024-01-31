01bc6462f608b2fc1707e5bcf8f93bfee94b29a1
zotico <thezotico@gmail.com>
2024-01-31 15:00:31 +0900

    make_DTOs

---
+++ src/main/java/com/groupb/cuiz/app/mypage/Purchase_ListDTO.java
@@ -0,0 +1,38 @@
+package com.groupb.cuiz.app.mypage;
+
+import java.sql.Date;
+
+public class Purchase_ListDTO {
+
+
+	public Integer product_Num;
+	public Integer member_ID;
+	public Date pl_List;
+	
+	
+	
+	
+	
+	public Integer getProduct_Num() {
+		return product_Num;
+	}
+	public void setProduct_Num(Integer product_Num) {
+		this.product_Num = product_Num;
+	}
+	public Integer getMember_ID() {
+		return member_ID;
+	}
+	public void setMember_ID(Integer member_ID) {
+		this.member_ID = member_ID;
+	}
+	public Date getPl_List() {
+		return pl_List;
+	}
+	public void setPl_List(Date pl_List) {
+		this.pl_List = pl_List;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/groupb/cuiz/app/mypage/ShopDTO.java
@@ -0,0 +1,50 @@
+package com.groupb.cuiz.app.mypage;
+
+import java.sql.Blob;
+
+public class ShopDTO {
+	
+	private Integer product_Num;
+	private String product_Name;
+	private String product_Contents;
+	private Integer product_Price;
+	private Blob product_Photo;
+	
+	
+	
+	
+	
+	public Integer getProduct_Num() {
+		return product_Num;
+	}
+	public void setProduct_Num(Integer product_Num) {
+		this.product_Num = product_Num;
+	}
+	public String getProduct_Name() {
+		return product_Name;
+	}
+	public void setProduct_Name(String product_Name) {
+		this.product_Name = product_Name;
+	}
+	public String getProduct_Contents() {
+		return product_Contents;
+	}
+	public void setProduct_Contents(String product_Contents) {
+		this.product_Contents = product_Contents;
+	}
+	public Integer getProduct_Price() {
+		return product_Price;
+	}
+	public void setProduct_Price(Integer product_Price) {
+		this.product_Price = product_Price;
+	}
+	public Blob getProduct_Photo() {
+		return product_Photo;
+	}
+	public void setProduct_Photo(Blob product_Photo) {
+		this.product_Photo = product_Photo;
+	}
+	
+	
+
+}
