c10f3fcfacd47c66a796aebccec5e83c2c3fc197
dongil <az330015@gmail.com>
2024-02-06 17:53:27 +0900

    FileManager 중복수정

--- src/test/java/com/groupb/cuiz/support/util/FileManagerTest.java
+++ src/test/java/com/groupb/cuiz/support/util/FileManagerTest.java
@@ -1,6 +1,7 @@
 package com.groupb.cuiz.support.util;
 
 import com.groupb.cuiz.MyTest;
+import com.groupb.cuiz.support.util.file.FileManager;
 import com.groupb.cuiz.web.quiz.QuizService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
