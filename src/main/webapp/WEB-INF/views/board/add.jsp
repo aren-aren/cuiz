59c1675cadbe722ee00391941d0cc6f8ba9dd2e4
dongil <az330015@gmail.com>
2024-02-28 10:17:30 +0900

    Quiz Add 할때 문제랑 링크하거나 코드를 첨부하는 기능 추가

--- src/main/webapp/WEB-INF/views/board/add.jsp
+++ src/main/webapp/WEB-INF/views/board/add.jsp
@@ -39,21 +39,29 @@
 							<div class="row">
 								<c:set var="b" value="${board}"></c:set>
 				                  <c:if test="${b eq 'QnA'}" >
+									  <div class="col-4">
+										  <input id="quiz-list-input" class="form-control" list="quiz-list" type="text" placeholder="링크할 문제 제목이나 번호를 검색하세요">
+										  <datalist id="quiz-list">
+										  </datalist>
+									  </div>
 									  <div class="col-3"></div>
-									  <div class="col-3"></div>
-									  <div class="col-3"></div>
-									  <div class="col-3">
-				                  		<button id="quiz-attach-btn" class="btn btn-cuiz float-end">QuizAttach</button>
+									  <div class="col-4">
+										  <button id="quiz-attach-btn" class="btn btn-cuiz float-end">코드 첨부</button>
 									  </div>
 				                  </c:if>
 									<div class="item heading-section" style= "margin-top: 5px">
 										<form id="contactForm" action="add" method="post" enctype="multipart/form-data">
+											<input type="hidden" id="quiz_No" name="quiz_No">
+											<input type="hidden" id="attach-code" name="flag" value="0">
 											<!-- title input-->
 											<div class="form-floating mb-3">
 												<input class="form-control" name="board_Title" id="name" type="text" placeholder="Title" data-sb-validations="required"/>
 												<label for="name">Title</label>
 												<div class="invalid-feedback" data-sb-feedback="message:required">필수항목 입니다.</div>
 											</div>
+
+
+
 											<!-- contents input-->
 											<div class="form-floating mb-3">
 												<textarea class="form-control" name="board_Contents" id="message" placeholder="Contents" style="height: 40rem" data-sb-validations="required"></textarea>
@@ -102,14 +110,14 @@
 			  </div>
 			  <div id="sampleRunModalBody" class="modal-body">
 				  <div class="mb-3">
-					  <input id="code-find-input" type="text" class="form-control">
+					  <input id="code-find-input" type="text" class="form-control" placeholder="검색할 문제의 제목을 입력해주세요">
 				  </div>
 				  <div id="code-content-div">
 
 				  </div>
 			  </div>
 			  <div class="modal-footer">
-				  <button id="addSubmit" type="button" class="btn btn-primary">선택하기</button>
+				  <button id="code-select-btn" type="button" class="btn btn-primary">선택하기</button>
 				  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
 			  </div>
 		  </div>
--- src/main/webapp/resources/js/board/add.js
+++ src/main/webapp/resources/js/board/add.js
@@ -1,7 +1,13 @@
 const quizAttachBtn = document.getElementById("quiz-attach-btn");
 const quizAttachModal = new bootstrap.Modal(document.getElementById("quiz-attach-modal"));
+const codeFindInput = document.getElementById("code-find-input");
+const codeSelectBtn = document.getElementById("code-select-btn");
+const codeContentDiv = document.getElementById("code-content-div");
+const quizListInput = document.getElementById("quiz-list-input");
+const quizList = document.getElementById("quiz-list");
 
 let codeData = [];
+let quizData = [];
 
 const dateFormat = date => {
     const year = date.getFullYear();
@@ -18,13 +24,12 @@ if (quizAttachBtn !== null) {
                     .then(r => r.json())
                     .then(r => {
                         console.log(r);
-                        const codeContentDiv = document.getElementById("code-content-div");
-
                         codeContentDiv.innerHTML = "";
 
+                        let inputs = ``;
                         for (const quizData of r) {
                             const inputTemplate = `
-<div class="row border-bottom m-2">
+<div class="row border-bottom m-2" data-title="${quizData.quiz_Title}">
     <div class="col-3 mb-0 p-3">
         <input id="quizNoRadio-${quizData.quiz_No}" class="form-check-input" type="radio" name="quiz_No" value="${quizData.quiz_No}">
     </div>
@@ -45,17 +50,61 @@ if (quizAttachBtn !== null) {
         </div>
     </label>
 </div>`
-                            codeContentDiv.innerHTML += inputTemplate;
+                            inputs += inputTemplate;
                         }
-
-                        console.log(codeContentDiv);
+                        codeContentDiv.innerHTML = inputs;
+                        codeData = [...codeContentDiv.children];
 
                         quizAttachModal.show();
                     })
             } else {
-
+                quizAttachModal.show();
             }
 
         }
     )
 }
+
+quizListInput.addEventListener("focus", ()=>{
+    if(quizData.length !== 0) return;
+
+    fetch("/quiz/getAllQuizs")
+        .then(r=>r.json())
+        .then(r=>{
+            console.log(r);
+            let str = "";
+            for (const quiz of r) {
+                str += `
+                <option value="${quiz.quiz_No}">${quiz.quiz_Title}</option>
+                `
+                quizData.push(r);
+            }
+            quizList.innerHTML = str;
+        })
+})
+
+quizListInput.addEventListener("change", event=>{
+    document.getElementById("quiz_No").value = event.target.value;
+})
+
+codeFindInput.addEventListener("keyup", event=>{
+    const str = event.target.value;
+    const regex = new RegExp(`${str}`);
+
+    codeData.forEach(e=>{
+        const title = e.getAttribute("data-title");
+
+        if(regex.test(title)) {
+            e.classList.remove("d-none");
+        } else {
+            e.classList.add("d-none");
+        }
+    })
+})
+
+codeSelectBtn.addEventListener("click", ()=>{
+    let checkedInput = codeContentDiv.querySelector("input[type=radio]:checked");
+    document.getElementById("quiz_No").value = checkedInput.value;
+    document.getElementById("attach-code").value = 1;
+    quizAttachModal.hide();
+})
